96aaca7f6298fa7b08fc5ccf1caaf9c2fb836b24
Marti de Castro <80928813+Marti-de-Castro@users.noreply.github.com>
2023-12-05 23:03:22 -0300

    Add files via upload

---
+++ projeto27/script/script.js
@@ -0,0 +1,39 @@
+function show_morango()
+{
+document.getElementById("morango").style.visibility="visible";
+document.getElementById("limao").style.visibility="hidden";
+document.getElementById("maca").style.visibility="hidden";
+document.getElementById("laranja").style.visibility="hidden";
+}
+
+function show_limao()
+{
+document.getElementById("morango").style.visibility="hidden";
+document.getElementById("limao").style.visibility="visible";
+document.getElementById("maca").style.visibility="hidden";
+document.getElementById("laranja").style.visibility="hidden";
+}
+
+function show_maca()
+{
+document.getElementById("morango").style.visibility="hidden";
+document.getElementById("limao").style.visibility="hidden";
+document.getElementById("maca").style.visibility="visible";
+document.getElementById("laranja").style.visibility="hidden";
+}
+
+function show_laranja()
+{
+document.getElementById("morango").style.visibility="hidden";
+document.getElementById("limao").style.visibility="hidden";
+document.getElementById("maca").style.visibility="hidden";
+document.getElementById("laranja").style.visibility="visible";
+}
+
+function some_tudo()
+{
+document.getElementById("morango").style.visibility="hidden";
+document.getElementById("limao").style.visibility="hidden";
+document.getElementById("maca").style.visibility="hidden";
+document.getElementById("laranja").style.visibility="hidden";
+}
